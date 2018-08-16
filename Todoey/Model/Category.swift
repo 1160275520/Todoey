63d11ae277c881d4f5e20f3fb4fa756726a3ea69
1160275520 <wangy3@carleton.edu>
2018-08-16 15:45:24 -0500

    apply realm database

---
+++ Todoey/Model/Category.swift
@@ -0,0 +1,17 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Mac Air on 2018/8/16.
+//  Copyright © 2018 Yiwen Wang. All rights reserved.
+//
+
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
+
